9f1fcad2a4a54f49e1e6442006b3d31d229543d1
pai_glb <pai_hi_glb@163.com>
2020-05-29 11:55:14 +0800

    v1.0

---
+++ src/main/java/se/zuse/App.java
@@ -0,0 +1,13 @@
+package se.zuse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello, this is my first try!" );
+    }
+}
