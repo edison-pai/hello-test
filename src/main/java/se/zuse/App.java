cf3326d3a8e12539fec4f3c39bf01765c44ad358
pai_glb <pai_hi_glb@163.com>
2020-05-29 12:02:18 +0800

    v2.0

--- src/main/java/se/zuse/App.java
+++ src/main/java/se/zuse/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello, this is my first try!" );
+//        System.out.println( "Hello, this is my first try!" );
+        System.out.println("Hi,what is your name?");
+        User user =new User();
+        user.setName("张三");
+        System.out.println("My name is "+user.getName());
     }
 }
